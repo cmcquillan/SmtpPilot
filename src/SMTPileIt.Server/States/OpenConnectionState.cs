f68e89a3e6f19f68268d1bc18e8b22593055e2ad
Casey McQuillan <casey.mcquillan@gmail.com>
2015-07-02 15:51:49 -0400

    Adding new files to solution.

---
+++ src/SMTPileIt.Server/States/OpenConnectionState.cs
@@ -0,0 +1,33 @@
+﻿using SMTPileIt.Server.Conversation;
+using SMTPileIt.Server.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMTPileIt.Server.States
+{
+    public class OpenConnectionState : IConversationState
+    {
+        public void EnterState(IMailClient client)
+        {
+            client.Write(new SmtpReply(SmtpReplyCode.Code220).GetReply());
+        }
+
+        public IConversationState Process(ISmtpStateContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LeaveState(ISmtpStateContext context)
+        {
+            
+        }
+
+        public SmtpCommand AllowedCommands
+        {
+            get { return Conversation.SmtpCommand.EHLO | Conversation.SmtpCommand.HELO; }
+        }
+    }
+}checked
