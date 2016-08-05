46c7c340c47f6e4d1bf31da814272d7b5032001e
Casey McQuillan <casey.mcquillan@gmail.com>
2016-08-05 10:35:22 -0700

    Added graceful shutdown to console server by calling SMTPServer.Stop().

--- src/SmtpPilot/Program.cs
+++ src/SmtpPilot/Program.cs
@@ -10,31 +10,41 @@ namespace SmtpPilot
 {
     class Program
     {
+        private static SMTPServer _server;
+
         static void Main(string[] args)
         {
+            Console.CancelKeyPress += Console_CancelKeyPress;
+
             var config = new SmtpPilotConfiguration("127.0.0.1", 25)
             {
                 ClientTimeoutSeconds = 1000,
                 MailStore = new XmlMailStore(),
             };
 
-            var server = new SMTPServer(config);
+            _server = new SMTPServer(config);
 
             ConsoleHooks.LogInfo("Starting mock SMTP server.");
 
-            server.ClientConnected += ConsoleHooks.Server_ClientConnected;
-            server.ClientDisconnected += ConsoleHooks.Server_ClientDisconnected;
-            server.EmailProcessed += ConsoleHooks.Server_EmailProcessed;
-            server.ServerStarted += ConsoleHooks.Server_Started;
-            server.ServerStopped += ConsoleHooks.Server_Stopped;
-            server.Start();
+            _server.ClientConnected += ConsoleHooks.Server_ClientConnected;
+            _server.ClientDisconnected += ConsoleHooks.Server_ClientDisconnected;
+            _server.EmailProcessed += ConsoleHooks.Server_EmailProcessed;
+            _server.ServerStarted += ConsoleHooks.Server_Started;
+            _server.ServerStopped += ConsoleHooks.Server_Stopped;
+            _server.Start();
 
             ConsoleHooks.LogInfo("Press 'q' to stop server.");
 
             while (Console.ReadKey(true).KeyChar != 'q') { }
 
             ConsoleHooks.LogInfo("Shutting down mock SMTP server.");
-            server.Stop();
+            _server.Stop();
+        }
+
+        private static void Console_CancelKeyPress(object sender, ConsoleCancelEventArgs e)
+        {
+            ConsoleHooks.LogInfo($"Received shutdown signal.");
+            _server.Stop();
         }
     }
 }
