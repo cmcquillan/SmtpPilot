77c978d0c6dd15283ef6b61110a3280f7c9e311b
Casey McQuillan <casey.mcquillan@gmail.com>
2016-08-05 16:30:58 -0700

    Adjusted console messages to include date and time.

--- src/SmtpPilot/ConsoleHooks.cs
+++ src/SmtpPilot/ConsoleHooks.cs
@@ -12,17 +12,17 @@ namespace SmtpPilot
     {
         internal static void Server_EmailProcessed(object sender, EmailProcessedEventArgs eventArgs)
         {
-            LogInfo($"Client processed mail:{Environment.NewLine}\tId: {eventArgs.ClientId} {Environment.NewLine}\tFrom: {eventArgs.Message.FromAddress}");
+            LogInfo($"Client processed mail.{Environment.NewLine}\tId: {eventArgs.ClientId} {Environment.NewLine}\tFrom: {eventArgs.Message.FromAddress}");
         }
 
         internal static void Server_ClientDisconnected(object sender, MailClientDisconnectedEventArgs eventArgs)
         {
-            LogInfo($"Client disconnected from server:{Environment.NewLine}\tId: {eventArgs.ClientId}{Environment.NewLine}\tReason: {eventArgs.Reason}");
+            LogInfo($"Client disconnected from server.{Environment.NewLine}\tId: {eventArgs.ClientId}{Environment.NewLine}\tReason: {eventArgs.Reason}");
         }
 
         internal static void Server_ClientConnected(object sender, MailClientConnectedEventArgs eventArgs)
         {
-            LogInfo($"Server received new connection:{Environment.NewLine}\tId: {eventArgs.ClientId}");
+            LogInfo($"Server received new connection.{Environment.NewLine}\tId: {eventArgs.ClientId}");
         }
 
         internal static void Server_Stopped(object sender, ServerEventArgs eventArgs)
@@ -37,7 +37,7 @@ namespace SmtpPilot
 
         internal static void LogInfo(string text)
         {
-            Console.WriteLine(text);
+            Console.WriteLine($"({DateTime.Now:O}): {text}");
         }
     }
 }
