3d7548de394756ade68c41a9b403fb551168b4f0
Casey McQuillan <casey.mcquillan@gmail.com>
2017-02-03 14:16:29 -0800

    Flipping back bad change from ASCII to UTF8.  Need to research how to use UTF8 encoding in messages.

--- src/SmtpPilot.Server/IO/TcpMailClient.cs
+++ src/SmtpPilot.Server/IO/TcpMailClient.cs
@@ -38,8 +38,8 @@ namespace SmtpPilot.Server.IO
             _tcpClient = client;
             _clientId = clientId;
             _inputStream = _tcpClient.GetStream();
-            _reader = new StreamReader(_inputStream, Encoding.UTF8);
-            _writer = new StreamWriter(_inputStream, Encoding.UTF8, START_BUFFER_SIZE);
+            _reader = new StreamReader(_inputStream, Encoding.ASCII);
+            _writer = new StreamWriter(_inputStream, Encoding.ASCII, START_BUFFER_SIZE);
             _lastDataAvailable = DateTime.Now;
         }
 
