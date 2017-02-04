218fa2b68fb9729ba08dc6fdde59d74d2110bdc0
Casey McQuillan <casey.mcquillan@gmail.com>
2017-02-03 18:18:03 -0800

    Adding an in memory mail store which keeps a specified number of messages in memory.

---
+++ src/SmtpPilot.Server/Data/InMemoryMailStore.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SmtpPilot.Server.Conversation;
+
+namespace SmtpPilot.Server.Data
+{
+    public class InMemoryMailStore : IMailStore
+    {
+        public const int DefaultRetentionAmount = 100;
+        private readonly int _retentionAmount;
+        private readonly LinkedList<IMessage> _messages;
+
+        public InMemoryMailStore()
+            : this(DefaultRetentionAmount)
+        {
+        }
+
+        public InMemoryMailStore(int retentionAmount)
+        {
+            _retentionAmount = retentionAmount;
+            _messages = new LinkedList<IMessage>();
+        }
+
+        public void SaveMessage(IMessage message)
+        {
+            if (_messages.Count == _retentionAmount)
+                _messages.RemoveFirst();
+
+            _messages.AddLast(message);
+        }
+    }
+}
